5ec9dbbc07b93bf011d19190589275d3624a9515
himanshuCoder <himanshu.bijwar.bca@ghriet.raisoni.net>
2023-01-03 20:49:34 +0530

    Program to find Even and odd number

--- src/main/java/com/bootcoding/java/assigment/OddNumber.java
+++ src/main/java/com/bootcoding/java/assigment/OddNumber.java
@@ -1,11 +1,11 @@
-package assigment;
+package com.bootcoding.java.assigment;
 
 public class OddNumber {
     public static void main(String[] args) {
         int n=10;
         for(int i=2;i<=n;i++){
             if(i%2==0){
-                sout
+                System.out.println(i);
             }
         }
     }
