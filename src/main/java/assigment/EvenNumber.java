70439f76595d4230e9833f57a71cd1df416940c3
himanshuCoder <himanshu.bijwar.bca@ghriet.raisoni.net>
2023-01-03 10:47:56 +0530

    Program to find Even and odd number

---
+++ src/main/java/assigment/EvenNumber.java
@@ -0,0 +1,14 @@
+package assigment;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+
+        int n=10;
+
+        for(int i=2;i<=n;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/main/java/assigment/OddNumber.java
@@ -0,0 +1,12 @@
+package assigment;
+
+public class OddNumber {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=2;i<=n;i++){
+            if(i%2==0){
+                sout
+            }
+        }
+    }
+}
