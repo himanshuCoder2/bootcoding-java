c0bd36db4ae84559b544dab9bd8e3f39d8a0bd77
himanshuCoder <himanshu.bijwar.bca@ghriet.raisoni.net>
2023-01-05 11:15:56 +0530

    Using Method API with array  to create DiscountSystem

--- src/main/java/com/bootcoding/java/MethodAPI/DiscountSystem.java
+++ src/main/java/com/bootcoding/java/MethodAPI/DiscountSystem.java
@@ -2,10 +2,10 @@ package com.bootcoding.java.MethodAPI;
 
 public class DiscountSystem {
 
-    public static char[] calculateDiscount(int[] userPurchase) {
-        char[] discounts = new char[userPurchase.length];
+    public static double[] calculateDiscount(int[] userPurchase) {
+        double[] discounts = new double[userPurchase.length];
         for (int i = 0; i < userPurchase.length; i++) {
-            char discount;
+            double discount;
             int percentage = userPurchase[i];
             discount = getDiscount(percentage);
             discounts[i] = discount;
@@ -13,8 +13,8 @@ public class DiscountSystem {
         return discounts;
 
     }
-    private static  char getDiscount(int percentage) {
-        char discount;
+    private static  double getDiscount(int percentage) {
+        double discount;
         if (percentage > 8) {
             discount = 50;
         } else if (percentage >= 5) {
@@ -29,9 +29,11 @@ public class DiscountSystem {
 
         public static void main(String[]args){
         int[] userPurchase = {10, 5, 9, 1, 3, 7, 2, 0};
-        char[] discountResult =calculateDiscount((userPurchase));
+        double[] discountResult =calculateDiscount((userPurchase));
         for (int i=0; i<discountResult.length; i++ ) {
             System.out.println(userPurchase[i] + " -> " + discountResult[i]);
         }
     }
 }
+
+/* public  String */
